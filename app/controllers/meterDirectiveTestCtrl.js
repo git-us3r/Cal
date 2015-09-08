cc200c5a780b728c612cd3b32de45e8247dc0be9
Miguel Lopez <mlopez4@mmm.com>
2015-09-07 22:20:50 -0600

    setup master controller's parameters for dayCardDirective's api

--- app/controllers/meterDirectiveTestCtrl.js
+++ app/controllers/meterDirectiveTestCtrl.js
@@ -11,11 +11,42 @@
 
 		var vm = this;
 
-		vm.CurrentEvent = {
+		vm.DayEvents = [
 
-			Msg : "MeterBar directive below ...",
-			Start : moment({y: 2015, M: 9, d:2, H:7, m:0, s:0, ms:0}),
-        	End : moment({y: 2015, M: 9, d:3, H:0, m:0, s:0, ms:0})
+			{
+				id : 'Available',
+				start : moment({y: 2015, M: 9, d:2, H:7, m:0, s:0, ms:0}),
+        		end : moment({y: 2015, M: 9, d:2, H:11, m:0, s:0, ms:0})
+			},
+			{
+				id : 'Available',
+				start : moment({y: 2015, M: 9, d:2, H:13, m:0, s:0, ms:0}),
+        		end : moment({y: 2015, M: 9, d:2, H:16, m:0, s:0, ms:0})
+			},
+			{
+				id : 'Available',
+				start : moment({y: 2015, M: 9, d:2, H:19, m:0, s:0, ms:0}),
+        		end : moment({y: 2015, M: 9, d:2, H:23, m:0, s:0, ms:0})
+			},
+			{
+				id : 'Available',
+				start : moment({y: 2015, M: 9, d:3, H:7, m:0, s:0, ms:0}),
+        		end : moment({y: 2015, M: 9, d:3, H:23, m:0, s:0, ms:0})
+			},
+			{
+				id : 'Available',
+				start : moment({y: 2015, M: 9, d:4, H:7, m:0, s:0, ms:0}),
+        		end : moment({y: 2015, M: 9, d:4, H:13, m:0, s:0, ms:0})
+			}
+		];
+
+		vm.CurrentEvent = vm.DayEvents[0];
+
+		vm.Msg = 'Directive Test Ground';
+
+		vm.ViewEvent = function() {
+
+			// TODO
 		};
 
         return vm;

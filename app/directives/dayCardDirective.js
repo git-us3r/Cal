f95981d2b4c95518cc147decd5d2b58360285029
Miguel Lopez <mlopez4@mmm.com>
2015-09-09 16:01:43 -0600

    made the meters in dayCardDirective behave nicely, wrt positioning. TODO: the new approach seems cleaner than the one used in the event meter directive - implement.

--- app/directives/dayCardDirective.js
+++ app/directives/dayCardDirective.js
@@ -47,11 +47,18 @@
 
 		angular.forEach(localScope.events, function(value, key){
 			
-			value.height = getEventHeightAsPercentage(value);
+			value.top = getEventTop(value);
+			value.bottom = getEventBottom(value);
+			value.height = getEventHeight(value);
+
+			if(value.top + value.bottom + value.height > 100) {
+
+				throw 'incorrect dimensions for task meter';
+			}
 		});
 	}
 
-	function getEventHeightAsPercentage(_event) {
+	function getEventHeight(_event) {
 
 		var workDayInMinutes = 17 * 60,
 			eventEndInMinutes = _event.end.hours() === 0 ? 24 : _event.end.hours() * 60 + _event.end.minutes(),
@@ -61,6 +68,35 @@
 			eventHeightAsPercentage = eventDurationInMinutes / workDayInMinutes * maxHeight;
 
 		return Math.floor(eventHeightAsPercentage);
+	}
+
+	function getEventTop(_event) {
+
+		var workDayInMinutes = 17 * 60,
+			maxHeight = 100,
+			startOfDayInMinutes = 7 * 60,
+			eventStartInMinutes = _event.start.hours() * 60 + _event.start.minutes(),
+			minutesFromStartOfDay = eventStartInMinutes - startOfDayInMinutes,
+			top = minutesFromStartOfDay / workDayInMinutes * maxHeight;
+
+		return Math.floor(top);
+	}
+
+	function getEventBottom(_event) {
+
+		if(_event.end.hours() === 0) {
+
+			return 0;
+		}
+
+		var workDayInMinutes = 17 * 60,
+			maxHeight = 100,
+			endOfDayInMinutes = 24 * 60,
+			eventEndInMinutes = _event.end.hours() * 60 + _event.end.minutes(),
+			minutesFromEndOfDay = endOfDayInMinutes - eventEndInMinutes,
+			top = minutesFromEndOfDay / workDayInMinutes * maxHeight;
+
+		return Math.floor(top);
 	}
 
 
